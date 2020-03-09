c2c3516b313a41979c8896c70b9de9904db03540
mfauzanalg <mfauzanalg@gmail.com>
2020-03-09 21:29:29 +0700

    Update ReadFile.cs

--- CoronaSimulation/ReadFile.cs
+++ CoronaSimulation/ReadFile.cs
@@ -5,31 +5,13 @@ namespace ReadFile
     {
         public static void Read()
         {
-            // The files used in this example are created in the topic
-            // How to: Write to a Text File. You can change the path and
-            // file name to substitute text files of your own.
-
-            // System.Console.WriteLine("Example 1");
-            // // Example #1
-            // // Read the file as one string.
-            // string text = System.IO.File.ReadAllText(@"C:\Users\FAUZAN\Documents\Semester 4\1. Stima\Tubes-2-Stima\CoronaSimulation\text1.txt");
-
-            // // Display the file contents to the console. Variable text is a string.
-            // System.Console.WriteLine("Contents of text1.txt = {0}", text);
-
-            // System.Console.WriteLine("Example 2");
-            // Example #2
-            // Read each line of the file into a string array. Each element
-            // of the array is one line of the file.
-            string[] files1 = System.IO.File.ReadAllLines(@"C:\Users\FAUZAN\Documents\Semester 4\1. Stima\Tubes-2-Stima\CoronaSimulation\text1.txt");
-            string[] files2 = System.IO.File.ReadAllLines(@"C:\Users\FAUZAN\Documents\Semester 4\1. Stima\Tubes-2-Stima\CoronaSimulation\text2.txt");
+            string[] files1 = System.IO.File.ReadAllLines(@"..\..\..\text1.txt");
+            string[] files2 = System.IO.File.ReadAllLines(@"..\..\..\text2.txt");
 
 
-            // Display the file contents by using a foreach loop.
             System.Console.WriteLine("Contents of text1.txt = ");
             foreach (string line1 in files1)
             {
-                // Use a tab to indent each line of the file.
                 Console.WriteLine(line1);
             }
 
@@ -40,16 +22,13 @@ namespace ReadFile
             for (int i = 1; i < nEdge; i++){
                 string[] edges = files1[i].Split(' ');
                 foreach (string edge in edges){
-                    // Use a tab to indent each line of the file.
                     Console.WriteLine(edge);
                 }
             }
 
-
             System.Console.WriteLine("Contents of text2.txt = ");
             foreach (string line2 in files2)
             {
-                // Use a tab to indent each line of the file.
                 Console.WriteLine(line2);
             }
 
