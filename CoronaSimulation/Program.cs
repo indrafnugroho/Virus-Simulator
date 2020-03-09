da6584c831fde36923ce5594986262a009da5fd9
Indra Nugroho <indrafbrngrh@gmail.com>
2020-03-09 19:34:58 +0700

    add files c#

---
+++ CoronaSimulation/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace CoronaSimulation
+{
+    class Program
+    {
+        double Gamma = 0.25;
+        public static void Main()
+        {
+            Console.Write("Input Total Days Infected: ");
+            int T = Convert.ToInt32(Console.ReadLine());
+
+
+            
+            Console.WriteLine("Hello World!");
+        }
+
+        public double InfectedPopulation (int Population, int T)
+        {
+
+        }
+    }
+}
