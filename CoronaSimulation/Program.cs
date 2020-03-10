d6501a6659f598c937cc2ca276e7e4600aae2070
mfauzanalg <mfauzanalg@gmail.com>
2020-03-10 07:25:16 +0700

    Nambahin Queue

--- CoronaSimulation/Program.cs
+++ CoronaSimulation/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Entity;
 using ReadFile;
+using System.Collections.Generic;
 
 namespace CoronaSimulation
 {
@@ -8,9 +9,20 @@ namespace CoronaSimulation
     {
         public static void Main()
         {
+            ReadFromFile.Read();
+            Queue<Tuple<char, char, double>> qEdge = new Queue<Tuple<char, char, double>>();
+
             Console.Write("Input Total Days Infected: ");
             int time = Convert.ToInt32(Console.ReadLine());
-            ReadFromFile.Read();
+            Console.WriteLine(ReadFromFile.GraphData[0].Item1); //cuma nyoba akses
+
+            //coba masukin ke queue tapi masih blm bisa akses si elemen sourcenya jadi masih coba pake A
+            foreach (Tuple<char, char, double> value in ReadFromFile.GraphData.FindAll(element => (element.Item1 == 'A'))) 
+            {
+                qEdge.Enqueue(value);
+            }
+
+            Console.WriteLine($"isi elemen pertama : {qEdge.Dequeue().Item1} -> {qEdge.Dequeue().Item2} ");
         }
     }
 }
