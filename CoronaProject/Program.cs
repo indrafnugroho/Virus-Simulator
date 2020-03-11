60e7e37956d081d7a968123040dc89e4a69b4a21
mfauzanalg <mfauzanalg@gmail.com>
2020-03-11 12:06:03 +0700

    Yes bisa visualisasi

---
+++ CoronaProject/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using Entity;
+using Visualization;
+using ReadFile;
+using System.Collections.Generic;
+using AlgCompute;
+
+namespace CoronaProject
+{
+    public class Simulation
+    {
+        public static void Main()
+        {
+            ReadFromFile.Read();
+            Queue<char> QueueEdge = new Queue<char>();
+
+            Console.Write("Input Total Days Infected: ");
+            int time = Convert.ToInt32(Console.ReadLine());
+            //ReadFromFile.EdgeData is data which contains Data of Edge
+            //ReadFromFile.GraphData is data which contains Data of Graph
+
+            //Init Graph G sudah di ReadFile
+            ReadFromFile.province.printGraph();
+            BFS.BFSCompute(time,ReadFromFile.source);
+            BFS.printSol(time);
+            Visualisasi.Visual();
+        }
+    }
+}
